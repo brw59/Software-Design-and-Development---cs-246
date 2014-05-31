a698bad0e1ea3100bcea4e02c6cd6c922a2d241c
Benjamin Ray Walker <benjaminraywalker@Benjamins-MacBook-Pro-2.local>
2014-05-30 23:19:07 -0600

    boldness and threads nearly completed
    I have nearly completed the exploration … please learn from them, don’t
    just take from them. It represents many many hours or research and
    guidance from Bro burton and friends in the class

--- daily dose of Java/Threads.java
+++ daily dose of Java/Threads.java
@@ -1,16 +1,47 @@
-class Threads {
-    public static void main(String[] args) {
-	Thread output1 = new Thread(new Output1());
-	output1.start();
+import javafx.application.Platform;
 
-	try {
-	    for (int i = 0; i < 100; i++)
-		{
-		    System.out.println(i);
-		    Thread.sleep(100);
-		}
-	} catch (Exception e) {
-	    System.out.println(e.getMessage());
-	}
+class Threads extends Thread { //implements Runnable {
+
+//	public boolean isFinished = false;
+	Thread thread;
+
+    public static void main(String[] args) {
+    	new Threads().run();
     }
+
+    @Override
+    public void run() {
+		thread = new Thread(new Output1());
+		thread.start();
+
+		try {
+	    	for (int i = 0; i <= 100 && Boldness.getInstance().shouldCurrentThreadBeRunning(); i++)
+			{
+		    	System.out.println(i);
+		    	Thread.sleep(100);
+			}
+		} catch (Exception e) {
+	   		System.out.println(e.getMessage());
+		}
+
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				Boldness.getInstance().getStage().setTitle("Popcorn Popping on the Apricot Tree!");
+			}
+ 		});
+
+		try {
+		Thread.sleep(5000);
+		} catch (Exception e) {
+			System.out.println("No sleepy for you...zzzz");
+		}
+
+ 		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				Boldness.getInstance().getStage().setTitle("Glory, Glory, Hallelujah!");
+			}
+ 		});
+	}
 }
